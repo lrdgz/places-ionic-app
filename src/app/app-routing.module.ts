ba75c50d6babe0f94d660d960a9b770b39fee39a
Luis Rodriguez <lui.rodriguez@gcnewhorizons.net>
2020-08-14 17:18:06 -0300

    Add Project Structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,16 +2,19 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
-  },
-  {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
-  },
+  { path: '', redirectTo: 'places', pathMatch: 'full' },
+  { path: 'auth', loadChildren: () => import('./auth/auth.module').then( m => m.AuthPageModule)},
+  { path: 'places', loadChildren: () => import('./places/places.module').then( m => m.PlacesPageModule) },
+  { path: 'bookings', loadChildren: () => import('./bookings/bookings.module').then( m => m.BookingsPageModule)},
 ];
+
+
+//   { path: 'discover', loadChildren: () => import('./places/discover/discover.module').then( m => m.DiscoverPageModule) },
+//   { path: 'offers', loadChildren: () => import('./places/offers/offers.module').then( m => m.OffersPageModule) },
+//   { path: 'new-offer', loadChildren: () => import('./places/offers/new-offer/new-offer.module').then( m => m.NewOfferPageModule) },
+//   { path: 'edit-offer', loadChildren: () => import('./places/offers/edit-offer/edit-offer.module').then( m => m.EditOfferPageModule) },
+//   { path: 'place-detail', loadChildren: () => import('./places/discover/place-detail/place-detail.module').then( m => m.PlaceDetailPageModule) },
+//   { path: 'offer-bookings', loadChildren: () => import('./places/offers/offer-bookings/offer-bookings.module').then( m => m.OfferBookingsPageModule) },
 
 @NgModule({
   imports: [
