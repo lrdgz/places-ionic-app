fcc0ad1288e7728bb5bff5f4f3e12323964313a2
Luis Rodriguez <lui.rodriguez@gcnewhorizons.net>
2020-08-14 17:50:08 -0300

    Add More Settings route ann Structure

---
+++ src/app/models/place.model.ts
@@ -0,0 +1,11 @@
+export class Place {
+    constructor (
+        public id: String, 
+        public title: String, 
+        public description: String,
+        public imageUrl: String,
+        public price: number,
+    ){
+        
+    }
+}
